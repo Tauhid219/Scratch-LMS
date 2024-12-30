d02f5137cba21e82371874d41b3e3a6d70559004
Hrakash79 <dev.hrakash@gmail.com>
2024-12-30 16:20:19 +0600

    change the file structure

---
+++ resources/views/frontend/courses.blade.php
@@ -0,0 +1,52 @@
+<div class=''>
+    <h1 class="md:text-5xl text-3xl text-center font-bold md:mt-20 mt-10 pb-10">
+        Courses
+    </h1>
+    <div class="md:grid grid-cols-2 gap-10 container mx-auto p-3">
+        <div
+            class="relative flex flex-col bg-clip-border rounded-xl text-gray-700 shadow-md overflow-hidden bg-slate-50 border p-4 hover:shadow-lg hover:shadow-orange-400">
+            <div
+                class="relative bg-clip-border overflow-hidden bg-transparent text-gray-700 shadow-none m-0 rounded-none">
+                <div class="relative h-64 rounded overflow-hidden">
+                    <img src="https://scratch.mit.edu/images/scratch-og.png" alt="{card.title}"
+                        class="w-full h-full object-cover transition-transform duration-300 hover:scale-110" />
+                </div>
+            </div>
+            <div class="md:flex justify-between items-center">
+                <div class="text-center text-black md:text-left">
+                    <h1 class="my-3 md:text-xl text-lg font-semibold uppercase">
+                        Scratch programming for everyone
+                    </h1>
+                    <p>Categories: Scratch Basic</p>
+                    <p class="text-sm opacity-40">video tutorial | Paid</p>
+                </div>
+                <div class="flex justify-center items-center">
+                    <a class="btn text-xl btn-primary" href="/">Start Now</a>
+                </div>
+            </div>
+        </div>
+        <div
+            class="relative flex flex-col bg-clip-border rounded-xl text-gray-700 shadow-md overflow-hidden md:mt-0 mt-5 bg-slate-50 border p-4 hover:shadow-lg hover:shadow-orange-400">
+            <div
+                class="relative bg-clip-border overflow-hidden bg-transparent text-gray-700 shadow-none m-0 rounded-none">
+                <div class="relative h-64 rounded overflow-hidden">
+                    <img src="https://www.learningresources.co.uk/media/home_uk/lruk-blog-images/What-is-Block-Coding-for-Kids_Header.jpg"
+                        alt="{card.title}"
+                        class="w-full h-full object-cover transition-transform duration-300 hover:scale-110" />
+                </div>
+            </div>
+            <div class="md:flex justify-between items-center">
+                <div class="text-center text-black md:text-left">
+                    <h1 class="my-3 md:text-xl text-lg font-semibold uppercase">
+                        Scratch programming for everyone
+                    </h1>
+                    <p>Categories: Scratch Basic</p>
+                    <p class="text-sm opacity-40">video tutorial | Paid</p>
+                </div>
+                <div class="flex justify-center items-center">
+                    <a class="btn text-xl btn-primary" href="/">Start Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/frontend/featureProjects.blade.php
@@ -0,0 +1,143 @@
+<div class="container mx-auto md:mt-20 mt-10 px-10">
+    <h1 class="my-3 md:text-5xl text-3xl text-center font-bold mb-10">
+        Featured Project
+    </h1>
+    <div class="md:grid grid-cols-4 gap-10">
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+        <div class="border bg-[#f2f2f2] p-4 hover:shadow-xl hover:border-2 shadow rounded-md mb-5">
+            <div class="">
+                <div class="w-full rounded">
+                    <img
+                        src="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2022/10/Screen-Shot-2022-10-11-at-1.40.36-PM.png" />
+                </div>
+            </div>
+            <div class="mt-5">
+                <h1 class="text-md font-semibold">
+                    Created By: Mustafizur Rahman
+                </h1>
+                <p class="text-md">
+                    Scratch Profile:
+                    <span class="link-primary">Musta12221</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
